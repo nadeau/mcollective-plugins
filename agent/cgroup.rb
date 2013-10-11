cbf0e69f363bf942b663f719ee0a21a7303168ed
Billy Nadeau <bill@parasite.nadeau>
2013-10-11 11:04:20 -0400

    Added support for new /cgroup/lxc path

--- agent/cgroup.rb
+++ agent/cgroup.rb
@@ -9,6 +9,10 @@ module MCollective
                   :version     => "1.0",
                   :timeout     => 5
 
+      def startup_hook
+        @path = File.directory?('/cgroup/lxc') ? '/cgroup/lxc' : '/cgroup';
+      end
+
       action "list" do
         reply[:cgroups] = getcgroups
       end
@@ -16,9 +20,9 @@ module MCollective
       action "get" do
         key = request[:key]
         getcgroups.each do |cg|
-          if File.directory?("/cgroup/#{cg}")
+          if File.directory?("#{@path}/#{cg}")
             reply[cg] = Hash.new
-            File.open("/cgroup/#{cg}/#{key}", "r")		{ |f| reply[cg][key] = f.read.chomp }
+            File.open("#{@path}/#{cg}/#{key}", "r")		{ |f| reply[cg][key] = f.read.chomp }
           end
         end
       end
@@ -26,40 +30,40 @@ module MCollective
       action "set" do
         key = request[:key]
         getcgroups.each do |cg|
-          if File.directory?("/cgroup/#{cg}")
+          if File.directory?("#{@path}/#{cg}")
             reply[cg] = Hash.new
-            File.open("/cgroup/#{cg}/#{key}", "w")		{ |f| f.puts(request[:value]) }
-            File.open("/cgroup/#{cg}/#{key}", "r")		{ |f| reply[cg][key] = f.read.chomp }
+            File.open("#{@path}/#{cg}/#{key}", "w")		{ |f| f.puts(request[:value]) }
+            File.open("#{@path}/#{cg}/#{key}", "r")		{ |f| reply[cg][key] = f.read.chomp }
           end
         end
       end
 
       action "blkio" do
         getcgroups.each do |cg|
-          if File.directory?("/cgroup/#{cg}")
+          if File.directory?("#{@path}/#{cg}")
             reply[cg] = Hash.new
-            File.open("/cgroup/#{cg}/blkio.weight", "r")	{ |f| reply[cg][:weight] = f.read.chomp }
+            File.open("#{@path}/#{cg}/blkio.weight", "r")	{ |f| reply[cg][:weight] = f.read.chomp }
           end
         end
       end
 
       action "cpu" do
         getcgroups.each do |cg|
-          if File.directory?("/cgroup/#{cg}")
+          if File.directory?("#{@path}/#{cg}")
             reply[cg] = Hash.new
-            File.open("/cgroup/#{cg}/cpu.shares", "r")		{ |f| reply[cg][:shares] = f.read.chomp }
+            File.open("#{@path}/#{cg}/cpu.shares", "r")		{ |f| reply[cg][:shares] = f.read.chomp }
           end
         end
       end
 
       action "memory" do
         getcgroups.each do |cg|
-          if File.directory?("/cgroup/#{cg}")
+          if File.directory?("#{@path}/#{cg}")
             reply[cg] = Hash.new
-            File.open("/cgroup/#{cg}/memory.usage_in_bytes", "r")     { |f| reply[cg][:used]    = f.read.chomp.to_i / 1048576 }
-            File.open("/cgroup/#{cg}/memory.max_usage_in_bytes", "r") { |f| reply[cg][:max]     = f.read.chomp.to_i / 1048576 }
-            File.open("/cgroup/#{cg}/memory.limit_in_bytes", "r")     { |f| reply[cg][:limit]   = f.read.chomp.to_i / 1048576 }
-            File.open("/cgroup/#{cg}/memory.failcnt", "r")            { |f| reply[cg][:failcnt] = f.read.chomp }
+            File.open("#{@path}/#{cg}/memory.usage_in_bytes", "r")     { |f| reply[cg][:used]    = f.read.chomp.to_i / 1048576 }
+            File.open("#{@path}/#{cg}/memory.max_usage_in_bytes", "r") { |f| reply[cg][:max]     = f.read.chomp.to_i / 1048576 }
+            File.open("#{@path}/#{cg}/memory.limit_in_bytes", "r")     { |f| reply[cg][:limit]   = f.read.chomp.to_i / 1048576 }
+            File.open("#{@path}/#{cg}/memory.failcnt", "r")            { |f| reply[cg][:failcnt] = f.read.chomp }
           end
         end
       end
@@ -68,7 +72,7 @@ module MCollective
         if request.include?(:cgroup) then
           [ request[:cgroup] ]
         else
-          Dir.chdir("/cgroup")
+          Dir.chdir("#{@path}")
           Dir.glob("*/").map{ |x| x.chop }
         end
       end
